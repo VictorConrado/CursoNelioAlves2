cfdef92469160534ffe7a059b4a93b58581a3051
VictorConrado <estudantecomv@gmail.com>
2024-09-08 23:26:29 -0300

    Curso Nelio Alvez 2 Parte

---
+++ CursoNelioAlves2/Entities/SavingAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoNelioAlves2.Entities
+{
+    internal class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }    
+
+        public SavingAccount() { }
+        public SavingAccount(int number, string holder, double balance, double interestRate)
+         : base(number, holder, balance)
+        {
+            InterestRate = interestRate ;
+        }
+
+        public void updateBalance() 
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
---
+++ CursoNelioAlves2/Program.cs
@@ -0,0 +1,43 @@
+﻿using CursoNelioAlves2.Entities;
+
+namespace CursoNelioAlves2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Account acc1 = new Account(1001, "Alex", 0.0);
+          BusinessAccount bacc = new BusinessAccount(1002, "Fray", 0.0, 500.0);
+
+           
+            
+            
+            
+            
+            ////UPCASTING
+
+            //Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            //Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
+
+            ////DOWNCASTING
+
+            //BusinessAccount acc4 = acc2 as BusinessAccount;
+            //acc4.Loan(100.0);
+            
+            ////BusinessAccount acc5 = acc3 as BusinessAccount;
+
+            //if(acc3 is BusinessAccount) 
+            //{
+            //  BusinessAccount acc5 = acc3 as BusinessAccount;
+            //    acc5.Loan(200.0);
+            //    Console.WriteLine("Loan!");
+            //}
+            //if (acc3 is SavingAccount)
+            //{
+            //    SavingAccount acc5 = acc3 as SavingAccount;
+            //    acc5.updateBalance();
+            //    Console.WriteLine("Update!");
+            //}
+        }
+    }
+}
